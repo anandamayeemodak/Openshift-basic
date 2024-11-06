f8736d85638e619f3c98c16eba9d28890afadb96
anandamayeemodak <amayee.modak@gmail.com>
2024-11-06 13:30:12 +0530

    replaced main.py

--- src/app.py
+++ src/app.py
@@ -11,4 +11,4 @@ async def baseRoute():
     return "Base Request ID: {}".format(str(id))
 
 if __name__=="__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
